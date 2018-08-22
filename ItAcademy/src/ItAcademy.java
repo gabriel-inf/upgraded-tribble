de5c938adb6a668f9eb47fc52699cdb2833b9078
batata123 <potato123@gmail.com>
2018-08-22 15:56:54 -0300

    added something

--- ItAcademy/src/ItAcademy.java
+++ ItAcademy/src/ItAcademy.java
@@ -1,4 +1,12 @@
 
 public class ItAcademy {
+	private String ano_turma;
+	private String nome;
+	private String pojeto; 
 
+
+	public void setAnoTurma(String ano_tuma){
+		this.ano_turma = ano_turma; 
+	}
+	
 }
